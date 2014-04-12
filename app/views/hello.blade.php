6b86ba0159f2977d82d76b583fd711d72b6f54ad
Tonnius <tonnius@gmail.com>
2014-04-12 15:34:20 +0300

    trying socket.io

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -11,4 +11,21 @@
 	<p>Oled jõudnud esialgsele ee_math_test leheküljele.</p>
 	
 	<p>Peatselt tekivad siia esmased leheküljed</p>
+	<script src='//code.jquery.com/jquery-1.7.2.min.js'></script>
+	<script src="http://localhost:40013/socket.io/socket.io.js"></script>
+	<script>
+	  var socket = io.connect('http://localhost:40013');
+			socket.on('news', function (data) {
+	    console.log(data);
+	    socket.emit('my other event', { my: 'data' });
+	    $('#messages').append('<li>' + data.hello + '</li>');
+	  });
+	                
+
+	                
+
+	</script>
+	<body>
+	        <ul id='messages'></ul>
+	    </body>
 @endsection
