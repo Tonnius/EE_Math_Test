68e090df58d601c40b05c7e10aed802a2f560ea7
Tonnius <tonnius@gmail.com>
2014-05-20 12:00:54 +0300

    moar content (litsentsid kontrollitud)

--- app/views/teemaTemplate.blade.php
+++ app/views/teemaTemplate.blade.php
@@ -10,6 +10,11 @@
 
 @section('content')
 	<h1>{{$teema->name}}</h1>
-
-	<a href="/teemad/{{urlencode($teema->name)}}/ylesanded">Ülesanded</a>
+	@if($teema->name == 'Trigonomeetria')
+		<h3><a href="/teemad/{{urlencode($teema->name)}}/trigoMaterjalid">Tekstilised materjalid</a></h3>
+		<h3><a href="/teemad/{{urlencode($teema->name)}}/trigoVideoMaterjalid">Videomaterjalid</a></h3>
+		<h3><a href="/teemad/{{urlencode($teema->name)}}/trigoMangud">Mängud ja abistavad visualiseeringud</a></h3>
+	@endif
+	<h3><a href="/teemad/{{urlencode($teema->name)}}/ylesanded">Ülesanded</a></h3>
+	
 @endsection
