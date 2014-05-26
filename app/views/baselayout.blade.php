49898beb16a07a74823865c5bc62b5b5d32a4b66
Tonnius <tonnius@gmail.com>
2014-05-26 21:37:35 +0300

    kontakt and kuidasOppida active fix

--- app/views/baselayout.blade.php
+++ app/views/baselayout.blade.php
@@ -30,7 +30,7 @@
 		</ul>
 	</li>
 
-	<li><a href="/kuidasOppida">Kuidas õppida</a></li>
+	<li @if (Request::is('kuidasOppida')) class="uk-active" @endif><a href="/kuidasOppida">Kuidas õppida</a></li>
 	<li @if (Request::is('kysiAbi')) class="uk-active" @endif><a href="/kysiAbi">Küsi abi</a></li>
 
 	<li class="uk-nav-divider"></li>
