11554490d99e264feeb9f5ac9950245634c50253
Tonnius <tonnius@gmail.com>
2014-05-26 22:27:11 +0300

    jimmi 2.1

--- app/views/kontakt.blade.php
+++ app/views/kontakt.blade.php
@@ -12,20 +12,20 @@
 	<h1>Kontaktandmed</h1>
 	
 	<div class="uk-panel uk-panel-box uk-panel-box-secondary">
- +	<h3>Administraatori kontaktinfo:</h3>
- +		<dl class="uk-description-list uk-description-list-horizontal">
- +			<dt>Pilt:</dt>
- +			<dd><img src="http://starcraft2.ingame.de/wiki/images/6/66/Char_raynor.png" alt="Jim Raynor" /></dd>
- +			<dt>Nimi:</dt>
- +			<dd>Jim Raynor</dd>
- +			<dt>Aadress:</dt>
- +			<dd>Hyperion Battlecruiser, somwhere near Korhal, Dominion Space</dd>
- +			<dt>Postiindeks:</dt>
- +			<dd>54A DEADZERG</dd>
- +			<dt>Telefon:</dt>
- +			<dd>0-800-YIPPIE-KI-YAY</dd>
- +			<dt>E-post:</dt>
- +			<dd>timeToManUp@gmail.com</dd>
- +		</dl>
- +	</div>
+ 	<h3>Administraatori kontaktinfo:</h3>
+ 		<dl class="uk-description-list uk-description-list-horizontal">
+ 			<dt>Pilt:</dt>
+ 			<dd><img src="http://starcraft2.ingame.de/wiki/images/6/66/Char_raynor.png" alt="Jim Raynor" /></dd>
+ 			<dt>Nimi:</dt>
+ 			<dd>Jim Raynor</dd>
+ 			<dt>Aadress:</dt>
+ 			<dd>Hyperion Battlecruiser, somwhere near Korhal, Dominion Space</dd>
+ 			<dt>Postiindeks:</dt>
+ 			<dd>54A DEADZERG</dd>
+ 			<dt>Telefon:</dt>
+ 			<dd>0-800-YIPPIE-KI-YAY</dd>
+ 			<dt>E-post:</dt>
+ 			<dd>timeToManUp@gmail.com</dd>
+ 		</dl>
+ 	</div>
 @endsection
